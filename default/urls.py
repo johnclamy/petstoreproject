824d3e490265448a4e48e9dd5fa3741c7119b0cc
johnclamy <johnclamy@gmail.com>
2025-08-07 10:20:39 +0100

    configure the default about us page url

--- default/urls.py
+++ default/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page
+from .views import about_us_page, home_page
 
 
 urlpatterns = [
     path('', home_page, name='default.home_page'),
+    path('about-us/', about_us_page, name='default.about_us_page'),
 ]
