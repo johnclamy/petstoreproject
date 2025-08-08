35ab8644cb570930800f1c24bcf931b4689d8ae8
johnclamy <johnclamy@gmail.com>
2025-08-08 08:47:32 +0100

    setup a template data dictionary and add to it  a page title

--- default/views.py
+++ default/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 
 
 def home_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'default/index.html')
+    template_data = {}
+    template_data['page_title'] = 'Welcome to Petzy Pet Store where you can find the pup of your dreams! | Home'
+
+    return render(request, 'default/index.html', {'data': template_data})
 
 
 def about_us_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'default/about-us.html')
+    template_data = {}
+    template_data['page_title'] = 'Find out all about our company and how we got here! | About Us'
+
+    return render(request, 'default/about-us.html', {'data': template_data})
