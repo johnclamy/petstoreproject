f04df99842fa606231c0b414e6e192b9d58d7e39
johnclamy <johnclamy@gmail.com>
2025-08-18 21:31:33 +0100

    configure the detail pet url

--- pets/urls.py
+++ pets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import pet_list_page
+from .views import pet_list_page, pet_detail_page
 
 
 urlpatterns = [
     path('', pet_list_page, name='pets.pet_list_page'),
+    path('<int:id>/', pet_detail_page, name='pets.pet_detail_page'),
 ]
