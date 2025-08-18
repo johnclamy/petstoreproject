88c4935230d56b3465363d2722939fd2e4bd1d18
johnclamy <johnclamy@gmail.com>
2025-08-18 17:18:53 +0100

    rename the view args to  pet_list_page in '/pets/urls.py'

--- pets/urls.py
+++ pets/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import pet_list_page
 
 
 urlpatterns = [
-    path('', index, name='pets.index'),
+    path('', pet_list_page, name='pets.pet_list_page'),
 ]
