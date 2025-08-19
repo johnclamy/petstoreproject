99fe7450a7dd6b251a64cf88440b39ab79d1d698
johnclamy <johnclamy@gmail.com>
2025-08-19 13:13:03 +0100

    correct the typo in the Pet class definition

--- pets/models.py
+++ pets/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Pet(models.Mode):
+class Pet(models.Model):
     id = models.AutoField(primary_key=True)
     breed = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
