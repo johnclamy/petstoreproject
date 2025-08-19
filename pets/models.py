3df18077ae590b614b945674ab5952c51c96f751
johnclamy <johnclamy@gmail.com>
2025-08-19 12:41:57 +0100

    create a pet model

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Pet(models.Mode):
+    id = models.AutoField(primary_key=True)
+    breed = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    is_kids_friendly = models.BooleanField(default=False)
+    description = models.TextField()
+    image = models.ImageField(upload_to='pet_images/')
+
+    def __str__(self):
+        return '{0} ({1}) - id: {3}'.format(self.breed, self.name, str(self.id))
+    
+
