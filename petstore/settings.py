e2d6f45c08fbbfe7e627c8bd7293cb423f4ab60f
johnclamy <johnclamy@gmail.com>
2025-08-19 15:19:39 +0100

    configure the server for image uploads

--- petstore/settings.py
+++ petstore/settings.py
@@ -118,3 +118,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'petstore/static/',
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
