9a09a7df17dc7f3488795988bb4429aba1ba00ec
johnclamy <johnclamy@gmail.com>
2025-08-21 23:25:01 +0100

    configure a logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register_page, signin_page
+from .views import register_page, signin_page, logout
 
 
 urlpatterns = [
     path('register', register_page, name='accounts.register_page'),
     path('signin/', signin_page, name='accounts.signin_page'),
+    path('logout/', logout, name='accounts.logout'),
 ]
