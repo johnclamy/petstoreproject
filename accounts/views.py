7e065890343825d2d8e30bdfb7058633ba01b719
johnclamy <johnclamy@gmail.com>
2025-08-21 12:01:38 +0100

    define the register function

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register_page(request: HttpRequest) -> HttpResponse:
+    template_data = {}
+    template_data['page_title'] = 'Register with Petzy to purchase a pet | Regsiter page'
+
+    if request.method == 'GET':
+        template_data['form'] = UserCreationForm()
+
+        return render(request, 'accounts/register.html', {'data': template_data })
