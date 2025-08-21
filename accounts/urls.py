19577d9995886a1a14081f8e7f9c19a8b5ee822c
johnclamy <johnclamy@gmail.com>
2025-08-21 20:41:43 +0100

    configure the sign in page url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register_page
+from .views import register_page, signin_page
 
 
 urlpatterns = [
     path('register', register_page, name='accounts.register_page'),
+    path('signin/', signin_page, name='accounts.signin_page'),
 ]
