06c858060635db352651e82e7b0294af1a8243a6
johnclamy <johnclamy@gmail.com>
2025-08-21 09:09:48 +0100

    order pets by breed on the django admin panel

--- pets/admin.py
+++ pets/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Pet
 
 
-admin.site.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    ordering = ['breed']
+
+
+admin.site.register(Pet, PetAdmin)
