1351a209101159c65c58e62f1a473c917631c0ec
johnclamy <johnclamy@gmail.com>
2025-08-21 22:13:04 +0100

    define a signin_page func in the '/accounts/views.py'

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from .forms import AccountsForm, Errors
 
 
@@ -20,3 +21,26 @@ def register_page(request: HttpRequest) -> HttpResponse:
         else:
             template_data['form'] = form
             return render(request, 'accounts/register.html', {'data': template_data })
+        
+
+def signin_page(request: HttpRequest) -> HttpResponse:
+    template_data = {}
+    template_data['page_title'] = 'Sign in to Petzy and purchase a pet | Sign in page'
+
+    if request.method == 'GET':
+        return render(request, 'accounts/signin.html', {'data': template_data})
+
+    elif request.method == 'POST':
+        user = authenticate(
+            request,
+            username = request.POST['username'],
+            password = request.POST['password']
+        )
+
+        if user is None:
+            template_data['error'] = 'Incorrect username or password.'
+            return render(request, 'accounts/signin.html', {'data': template_data})
+        else:
+            login(request, user)
+            return redirect('default.home_page')
+
