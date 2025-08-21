28447ae10ea752abe45b78abe80216becf72390f
johnclamy <johnclamy@gmail.com>
2025-08-21 17:15:49 +0100

    update register_page to use the custom MyUserCreationForm class

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import MyUserCreationForm
 
 
 def register_page(request: HttpRequest) -> HttpResponse:
@@ -8,11 +8,11 @@ def register_page(request: HttpRequest) -> HttpResponse:
     template_data['page_title'] = 'Register with Petzy to purchase a pet | Regsiter page'
 
     if request.method == 'GET':
-        template_data['form'] = UserCreationForm()
+        template_data['form'] = MyUserCreationForm()
         return render(request, 'accounts/register.html', {'data': template_data })
 
     elif request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
 
         if form.is_valid():
             form.save()
