354960d9c58a51c017a293d8752a6a24c46e1b5b
johnclamy <johnclamy@gmail.com>
2025-08-21 17:48:10 +0100

    customize the way errors are displayed

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms.utils import ErrorList
+from django.utils.safestring import mark_safe
+
+
+class Errors(ErrorList):
+    def __str__(self):
+        if not self:
+            return ''
+        
+        return mark_safe(''.join([
+            f'<div class="alert alert-danger" role="alert">{e}</div>' for e in self
+        ]))
 
 
 class MyUserCreationForm(UserCreationForm):
