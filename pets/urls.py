25f421a13f2fa68ac23312540b76319e8a962d77
johnclamy <johnclamy@gmail.com>
2025-08-22 16:48:31 +0100

    configure the create review url

--- pets/urls.py
+++ pets/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import pet_list_page, pet_detail_page
+from .views import create_review, pet_list_page, pet_detail_page
 
 
 urlpatterns = [
     path('', pet_list_page, name='pets.pet_list_page'),
     path('<int:id>/', pet_detail_page, name='pets.pet_detail_page'),
+    path('<int:id>/review/create/', create_review, name='pets.create_review'),
 ]
