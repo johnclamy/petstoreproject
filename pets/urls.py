9aa1fcfbfeee1ec80738fe74d426e5d259230f87
johnclamy <johnclamy@gmail.com>
2025-08-23 22:32:07 +0100

    configure the delete_review url

--- pets/urls.py
+++ pets/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import create_review, edit_review, pet_list_page, pet_detail_page
+from .views import create_review, delete_review, edit_review, pet_list_page, pet_detail_page
 
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:id>/', pet_detail_page, name='pets.pet_detail_page'),
     path('<int:id>/review/create/', create_review, name='pets.create_review'),
     path('<int:id>/review/<int:review_id>/edit/', edit_review, name='pets.edit_review'),
+    path('<int:id>/review/<int:review_id>/delete/', delete_review, name='pets.delete_review'),
 ]
