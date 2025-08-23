32821da5a859711715a7eaaea06f98dfa83a8950
johnclamy <johnclamy@gmail.com>
2025-08-23 18:55:53 +0100

    configure the edit_review url

--- pets/urls.py
+++ pets/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_review, pet_list_page, pet_detail_page
+from .views import create_review, edit_review, pet_list_page, pet_detail_page
 
 
 urlpatterns = [
     path('', pet_list_page, name='pets.pet_list_page'),
     path('<int:id>/', pet_detail_page, name='pets.pet_detail_page'),
     path('<int:id>/review/create/', create_review, name='pets.create_review'),
+    path('<int:id>/review/<int:review_id>/edit/', edit_review, name='pets.edit_review'),
 ]
