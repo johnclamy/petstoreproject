c6543e681c878453319e642d3f7898d89d3dff28
johnclamy <johnclamy@gmail.com>
2025-08-24 23:44:19 +0100

    configure the cart index url

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import add
+from .views import add, cart_home_page
 
 
 urlpatterns = [
+    path('', cart_home_page, name='cart.cart_home_page'),
     path('<int:id>/add/', add, name='cart.add'),
 ]
