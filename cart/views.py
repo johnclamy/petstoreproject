3bd1893c1c060d99b46c6a65f2e16c164018130b
johnclamy <johnclamy@gmail.com>
2025-08-24 21:59:45 +0100

    define the add function of the cart app

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from pets.models import Pet
 
-# Create your views here.
+
+def add(request: HttpRequest, id: int) -> HttpResponse:
+    get_object_or_404(Pet, id=id)
+    cart = request.session.get('cart', {})
+    
+    cart[id] = request.POST['quantity']
+    request.session['cart'] = cart
+
+    return redirect('default.home_page')
