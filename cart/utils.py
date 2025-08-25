d34a2b13fc419f81284da411ee273452a4fe0c8e
johnclamy <johnclamy@gmail.com>
2025-08-25 09:52:41 +0100

    calculate the total cost of the pets in the user’s cart

---
+++ cart/utils.py
@@ -0,0 +1,11 @@
+from pets.models import Pet
+
+
+def calculate_cart_total(cart: dict[str, str], pets_in_cart: list[Pet]) -> float:
+    total = 0
+
+    for pet in pets_in_cart:
+        quantity = cart[str[pet.id]]
+        total += pet.price * int(quantity)
+     
+    return total
