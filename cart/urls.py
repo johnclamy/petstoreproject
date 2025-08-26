e453db7a3b0ffe07117fb38ec1e459338650ecb2
johnclamy <johnclamy@gmail.com>
2025-08-27 00:32:00 +0100

    configure a clear url in the cart app

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import add, cart_home_page
+from .views import add, clear, cart_home_page
 
 
 urlpatterns = [
     path('', cart_home_page, name='cart.cart_home_page'),
     path('<int:id>/add/', add, name='cart.add'),
+    path('clear/', clear, name='cart.clear'),
 ]
