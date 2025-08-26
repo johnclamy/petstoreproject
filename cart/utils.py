daa9240b3c29920ca0eb52ed2bf2b8bbbe3001b8
johnclamy <johnclamy@gmail.com>
2025-08-26 22:52:22 +0100

    cast the int to str in the cart dictionary

--- cart/utils.py
+++ cart/utils.py
@@ -5,7 +5,7 @@ def calculate_cart_total(cart: dict[str, str], pets_in_cart: list[Pet]) -> float
     total = 0
 
     for pet in pets_in_cart:
-        quantity = cart[str[pet.id]]
+        quantity = cart[str(pet.id)]
         total += pet.price * int(quantity)
      
     return total
