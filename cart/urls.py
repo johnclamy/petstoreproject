338e96ee99234263a37f57e1e64885552403a689
johnclamy <johnclamy@gmail.com>
2025-08-27 20:55:52 +0100

    configure the purchase  url in cart app

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import add, clear, cart_home_page
+from .views import add, clear, cart_home_page, purchase
 
 
 urlpatterns = [
     path('', cart_home_page, name='cart.cart_home_page'),
     path('<int:id>/add/', add, name='cart.add'),
     path('clear/', clear, name='cart.clear'),
+    path('purchase/', purchase, name='cart.purchase'),
 ]
