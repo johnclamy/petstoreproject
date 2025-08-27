cf9c90cebe23b37302952eea04a96ea19c768332
johnclamy <johnclamy@gmail.com>
2025-08-27 16:26:20 +0100

    create the order model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Order {self.id}-{self.user.username}"
