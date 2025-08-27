11a2ee3da8a5b3f50c909305d24c4068c5162c3a
johnclamy <johnclamy@gmail.com>
2025-08-27 19:28:10 +0100

    create an Item model

--- cart/models.py
+++ cart/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from pets.models import Pet
 
 
 class Order(models.Model):
@@ -10,3 +11,14 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id}-{self.user.username}"
+
+
+class Item(models.Model):
+    id = models.AutoField(primary_key=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Item {self.id}-{self.pet.name}/{self.pet.breed}"
