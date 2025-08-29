5c7c54808b9651e877f5857304ead9dc521f6d9b
johnclamy <johnclamy@gmail.com>
2025-08-29 16:48:19 +0100

    add a db index for publish field

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,9 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-publish']  # define a default sort order for displaying posts
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
 
     def __str__(self):
         return self.title
