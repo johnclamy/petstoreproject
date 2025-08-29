82b1267994e32683b3a01fbfa73f9428bf86dbf4
johnclamy <johnclamy@gmail.com>
2025-08-29 08:21:37 +0100

    configure the orders url of the accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import register_page, signin_page, user_logout
+from .views import register_page, signin_page, user_logout, orders
 
 
 urlpatterns = [
     path('register', register_page, name='accounts.register_page'),
     path('signin/', signin_page, name='accounts.signin_page'),
     path('logout/', user_logout, name='accounts.user_logout'),
+    path('orders/', orders, name='accounts.orders'),
 ]
