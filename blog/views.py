3a56eb65804fe0542d41f5d82f2b876e6cc46609
johnclamy <johnclamy@gmail.com>
2025-09-01 13:36:13 +0100

    define the post detail page function in blog

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,16 @@ def post_list_page(request: HttpRequest) -> HttpResponse:
     template_data['posts'] = posts
 
     return render(request, 'blog/post/index.html', {'data': template_data})
+
+
+def post_detail_page(request: HttpRequest, id: int) -> HttpResponse:
+    try:
+        post = Post.published.get(id=id)       
+    except Post.DoesNotExist:
+        raise Http404('Post record not found.')
+
+    template_data = {}
+    template_data['page_title'] = 'Blog post titled "{0}" | blog detail page'.format(post.title)
+    template_data['post'] = post 
+
+    return render(request, 'blog/post/detail.html', {'data': template_data})
