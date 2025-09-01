d15fc46d14bf2906d11eb61855e215904956d9bd
johnclamy <johnclamy@gmail.com>
2025-09-01 13:32:11 +0100

    setup a view post_list_page function for the blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list_page(request: HttpRequest) -> HttpResponse:
+    template_data = {}
+    posts = Post.published.all()
+
+    template_data['page_title'] = 'Blog posts on Petzy | blog list page'
+    template_data['posts'] = posts
+
+    return render(request, 'blog/post/index.html', {'data': template_data})
