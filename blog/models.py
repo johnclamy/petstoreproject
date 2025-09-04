f62ec29f116c7007b06bc1588af7307c3c138d66
johnclamy <johnclamy@gmail.com>
2025-09-04 19:56:49 +0100

    build the canonical url for Post objects

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -32,3 +33,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail_page", args=[self.id])
+    
