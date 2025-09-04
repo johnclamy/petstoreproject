be591c89b183dcafb317534a778cd94b8def994a
johnclamy <johnclamy@gmail.com>
2025-09-04 22:58:59 +0100

    modify the url pattern to use the publication date and slug for the post detail url

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('post/', post_list_page, name='post_list_page'),
-    path('post/<int:id>/', post_detail_page, name='post_detail_page'),
+    path('post/<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail_page, name='post_detail_page'),
 ]
